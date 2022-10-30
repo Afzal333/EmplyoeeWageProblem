06953bf035ee9bcd6f88d72dee6c520005b84527
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-10-30 23:39:29 +0530

    [afzal] Add: PatTime Logic

--- EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -6,7 +6,7 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWage
     {
-        const int Wage_Per_Hr = 20, Full_Day_Hr = 8, Is_Present = 0, Is_Full_Time = 1;
+        const int Wage_Per_Hr = 20, Full_Day_Hr = 8, Is_Present = 0, Is_Full_Time = 1,Is_Part_Time=2, Part_Time_Hr =4;
         Random random = new Random();
         public void Attendance()
         {
@@ -19,9 +19,11 @@ namespace EmployeeWageProblem
         public void EmpWage()
         {
             int dailyEmpWage = 0, empHrs = 0;
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == Is_Full_Time)
                 empHrs = Full_Day_Hr;
+            else if (empCheck == Is_Part_Time)
+                empHrs = Part_Time_Hr;
             else
                 empHrs = 0;
             dailyEmpWage = Wage_Per_Hr * empHrs;
