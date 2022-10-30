d45c95004ee144dfcfa85265b7e9c77962336421
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-10-31 00:06:30 +0530

    [Afzal] Add : MonthlyWage

--- EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -6,7 +6,8 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWage
     {
-        const int Wage_Per_Hr = 20, Full_Day_Hr = 8, Is_Present = 0, Is_Full_Time = 1,Is_Part_Time=2, Part_Time_Hr =4;
+        const int Wage_Per_Hr = 20, Full_Day_Hr = 8, Is_Present = 0, 
+            Is_Full_Time = 1,Is_Part_Time=2, Part_Time_Hr =4,Total_Working_Days = 20;
         Random random = new Random();
         public void Attendance()
         {
@@ -18,19 +19,25 @@ namespace EmployeeWageProblem
         }
         public void EmpWage()
         {
-            int dailyEmpWage = 0, empHrs = 0;
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int TotalEmpWage = 0, empHrs = 0;
+            for (int i = 0; i < Total_Working_Days; i++)
             {
-                case Is_Full_Time: empHrs = Full_Day_Hr;
-                    break;
-                case Is_Part_Time: empHrs = Part_Time_Hr;
-                    break ;
-                default: empHrs = 0;
-                    break ;
-            }           
-            dailyEmpWage = Wage_Per_Hr * empHrs;
-            Console.WriteLine("Daily Emp Wage " + dailyEmpWage);
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case Is_Full_Time:
+                        empHrs += Full_Day_Hr;
+                        break;
+                    case Is_Part_Time:
+                        empHrs += Part_Time_Hr;
+                        break;
+                    default:
+                        empHrs += 0;
+                        break;
+                }
+            }
+            TotalEmpWage = Wage_Per_Hr * empHrs;
+            Console.WriteLine("Total Emp Wage " + TotalEmpWage);
         }
 
     }
